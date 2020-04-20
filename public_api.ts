e8a6609e2cfc22ebd78e786321b383fb039b924d
saurabh <saurabh.patel@mindinventory.com>
2020-04-20 22:41:34 +0530

    Update Screenfull version

--- public_api.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/app/image-viewer/image-viewer.module';
-export { ImageViewerComponent } from './src/app/image-viewer/image-viewer.component';
-export { ImageViewerConfig, CustomEvent } from './src/app/image-viewer/image-viewer-config.model';
